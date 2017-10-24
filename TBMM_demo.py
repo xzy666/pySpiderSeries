93d154c43329012d2f473d899b7539d2561aa1b5
xyz666 <504578726@qq.com>
2017-10-24 23:50:53 +0800

    淘宝妹妹 爬取

---
+++ TBMM_demo.py
@@ -0,0 +1,34 @@
+# coding:utf-8
+import requests
+import re
+
+result = requests.get('https://mm.taobao.com/json/request_top_list.htm?page=2')
+print result.text
+
+
+class TBMM_scrawer(object):
+    def __init__(self, url):
+        self.url = url
+
+    def getRawText(self, page=1):
+        result = requests.get(self.url % page)
+        return result.text
+
+    def getContent(self, page=1):
+        reg = re.compile(r'', re.DOTALL)
+        return re.findall(reg, self.getContent())
+
+    def start(self):
+        while True:
+            print '请输入要查看的页面,按Q退出'
+            page = raw_input()
+            if page == 'Q':
+                print 'Bye~'
+                return
+            for i in self.getContent(page):
+                print '-----------------------------------------------------------------------------'
+                print i
+
+
+T = TBMM_scrawer('ttps://mm.taobao.com/json/request_top_list.htm?page=%d')
+T.start()
