64cb77012e21a5fe80296f5b330a0f5c0165bded
xyz666 <504578726@qq.com>
2017-10-25 23:19:02 +0800

    淘宝妹妹 爬取

--- TBMM_demo.py
+++ TBMM_demo.py
@@ -17,7 +17,7 @@ class TBMM_scrawer(object):
 
     def getContent(self, page=1):
         reg = re.compile(
-            r'(<div class="list-item">.*?</div>)',
+            r'<div class="list-item".*?pic-word.*?<a href="(.*?)".*?<img src="(.*?)".*?<a class="lady-name.*?>(.*?)</a>.*?<strong>(.*?)</strong>.*?<span>(.*?)</span>',
             re.DOTALL)
         return re.findall(reg, self.getRawText())
 
@@ -30,6 +30,10 @@ class TBMM_scrawer(object):
                 return
             for i in self.getContent(page):
                 print '-----------------------------------------------------------------------------'
-                print i
+                print '主页','http:'+i[0]
+                print '头像','http:'+i[1]
+                print '姓名',i[2]
+                print '年龄',i[3]
+                print '城市',i[4]
 T = TBMM_scrawer('https://mm.taobao.com/json/request_top_list.htm?page=%d')
 T.start()
--- TieBar_demo.py
+++ TieBar_demo.py
@@ -1,7 +1,6 @@
 # coding:utf-8
 import requests
 import re
-import Tools
 
 
 # result = requests.get('http://tieba.baidu.com/p/3138733512?see_lz=1&pn=1')
@@ -52,7 +51,35 @@ class TieBar_Scrawer(object):
                 print '-----------------------------------------------------------------------------'
                 print '第', no, '楼'
                 no += 1
-                print Tools.Tools().replace(i)
+                print Tools().replace(i)
+
+
+class Tools(object):
+    # 去除img标签,7位长空格
+    removeImg = re.compile('<img.*?>| {7}|')
+    # 删除超链接标签
+    removeAddr = re.compile('<a.*?>|</a>')
+    # 把换行的标签换为\n
+    replaceLine = re.compile('<tr>|<div>|</div>|</p>')
+    # 将表格制表<td>替换为\t
+    replaceTD = re.compile('<td>')
+    # 把段落开头换为\n加空两格
+    replacePara = re.compile('<p.*?>')
+    # 将换行符或双换行符替换为\n
+    replaceBR = re.compile('<br><br>|<br>')
+    # 将其余标签剔除
+    removeExtraTag = re.compile('<.*?>')
+
+    def replace(self, x):
+        x = re.sub(self.removeImg, "", x)
+        x = re.sub(self.removeAddr, "", x)
+        x = re.sub(self.replaceLine, "\n", x)
+        x = re.sub(self.replaceTD, "\t", x)
+        x = re.sub(self.replacePara, "\n    ", x)
+        x = re.sub(self.replaceBR, "\n", x)
+        x = re.sub(self.removeExtraTag, "", x)
+        # strip()将前后多余内容删除
+        return x.strip()
 
 
 S = TieBar_Scrawer('http://tieba.baidu.com/p/3138733512?see_lz=1&pn=%d')
